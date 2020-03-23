c4131ec48962dfda2f513631a8dd52fa0849842b
Saurabh Gupta <saurabh.gupta@saurabhs-mbp.attlocal.net>
2020-03-23 10:25:50 -0500

    first commit

---
+++ venv/bin/jp.py
@@ -0,0 +1,4 @@
+#!/Users/saurabh.gupta/PycharmProjects/monitorCS/venv/bin/python
+# EASY-INSTALL-SCRIPT: 'jmespath==0.9.5','jp.py'
+__requires__ = 'jmespath==0.9.5'
+__import__('pkg_resources').run_script('jmespath==0.9.5', 'jp.py')
